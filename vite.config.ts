32d47f197bb0af40a39d2d7a04e0af3a7cada619
alps40411 <iainbo.lee@gmail.com>
2025-04-23 11:16:39 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/malaysia-i-genomics/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
